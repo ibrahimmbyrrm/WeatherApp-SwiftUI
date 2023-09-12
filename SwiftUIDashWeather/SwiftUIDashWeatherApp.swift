2a726ac3c2b59eb6cfd9833802292ecf68791036
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-09-12 15:14:32 +0300

    Initial Commit

---
+++ SwiftUIDashWeather/SwiftUIDashWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDashWeatherApp.swift
+//  SwiftUIDashWeather
+//
+//  Created by İbrahim Bayram on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDashWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
